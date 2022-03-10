ca7bff54bde07ee0cf388d64332bbb6757292dfa
kaspeya <89647596+kaspeya@users.noreply.github.com>
2022-03-10 14:36:07 +0300

    fixed line 79 in posts\views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -76,8 +76,7 @@ def post_edit(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     if request.user != post.author:
         return redirect('posts:post_detail', post_id)
-    else:
-        form = PostForm(request.POST or None, instance=post)
+    form = PostForm(request.POST or None, instance=post)
     if form.is_valid():
         form.save()
         return redirect('posts:post_detail', post_id)
